8e8c0ead9c6c2e8ac296da9ad3d51b1023da0d97
stefo <sfox@student.neumont.edu>
2015-07-07 16:29:08 -0600

    Lots of changes. The es6src folder will be built via babel and output into the standard src folder. It has been added to tracking for documentation purposes.

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,16 @@
 #! /usr/bin/env node
-console.log("you've started the program");
+// var colors = require('colors');
+var uitools = require('./src/ui/tools.js');
+
+/* main entry point */
+(function () {
+    // the module command-line-args demand the process.argv Array be uptampered.
+    // So I am forced into using all the functionality it supplies.
+    uitools.print("testprint", "cyan");
+    if(uitools.help) uitools.print(uitools.usage, "blue");
+    else {
+	for(k in uitools.getArgs) {
+	    uitools.print(uitools.getArgs[k], "green");
+	}
+    }
+})();
---
+++ es6src/index.js
@@ -0,0 +1,4 @@
+// node-cli-intro/src/index.js
+module.exports = function () {
+    console.log("This is how we do it.");
+}
