598a77c4b387813c6140ab890764b2f873344c64
Stephen Fox <stefo@SLL-1HCBMQ1.extendhealth.com>
2015-07-10 16:52:30 -0600

    Bump aliases don't work, so you will type them explicitly.

--- es6src/ui/tools.js
+++ es6src/ui/tools.js
@@ -8,8 +8,8 @@ export var cli = cliArgs([
     { name: "help", type: Boolean, alias: "h", description: "Print usage instructions" },
 //    { name: "files", type: Array, defaultOption: true, description: "The input files" },
     { name: "bump", type: Boolean, alias: "b", description: "Perform a 'patch' version bump" },
-    { name: "bump-major", type: Boolean, alias: "bma", description: "Perform a 'major' version bump" },
-    { name: "bump-minor", type: Boolean, alias: "bmi", description: "Perform a 'minor' version bump" },
+    { name: "bump-major", type: Boolean, description: "Perform a 'major' version bump" },
+    { name: "bump-minor", type: Boolean, description: "Perform a 'minor' version bump" },
     { name: "commit", type: Boolean, alias: "c", description: "Commit the local git repository." },
     { name: "message", type: String, alias: "m", description: "Message to supplied to a commit" }
 ]);
