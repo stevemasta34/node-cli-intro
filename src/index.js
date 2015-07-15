dcb9ea87633d96660500e9941d580d9bce278909
Stephen Fox <stevemasta34@gmail.com>
2015-07-15 17:35:02 -0600

    Mocha test commit:

--- src/index.js
+++ src/index.js
@@ -4,34 +4,35 @@ import { bumpVersion, commit } from "./io";
 
 export default function doFlow(optionsObj = cli.parse()) {
   if(optionsObj.help) { 
-    print(usage, "blue");
+    print(usage, "yellow");
   }
   else {
     console.log(optionsObj);
     let bumpCB = function(error, data) {
 	    // testing inappropriate bump function call
-	    console.log("Bump callback was hit");
+	    print(`Bump callback received data: ${data}`, "cyan");
     };
-    
-    // This could be done with string parsing, but that can be error pron
-    // and this is a definite thing that will reduce operation count
-    if (optionsObj["bump-major"] !== undefined) {
-	    // pass the major key to the bump command
-	    bumpVersion("./package.json", "major", bumpCB);
-    }
-    if (optionsObj["bump-minor"] !== undefined) {
-	    // pass the minor key to the bump command
-	    bumpVersion("./package.json", "minor", bumpCB);
-    }
-    if (optionsObj["bump-patch"] !== undefined) {
-	    // pass the patch key to the bump command
-	    bumpVersion("./package.json", "patch", bumpCB);
-    }
-    if (optionsObj.commit){
-	    commit(optionsObj.message, function(error, dat) {
-	      print(`The data thing: ${dat}`, "blue");
-	    });
 
+    if (optionsObj.message) {
+      
+      if (optionsObj.commit){
+	      commit(optionsObj.message, function(error, dat) {
+	        print(`The data thing: ${dat}`, "cyan");
+	      });
+      }
+      
+      if (optionsObj["bump-major"] !== undefined) {
+	      // pass the major key to the bump command
+	      bumpVersion("./package.json", "major", bumpCB);
+      }
+      if (optionsObj["bump-minor"] !== undefined) {
+	      // pass the minor key to the bump command
+	      bumpVersion("./package.json", "minor", bumpCB);
+      }
+      if (optionsObj["bump-patch"] !== undefined) {
+	      // pass the patch key to the bump command
+	      bumpVersion("./package.json", "patch", bumpCB);
+      }
     }
   }
 };
--- src/io.js
+++ src/io.js
@@ -9,23 +9,31 @@ import { exec } from "child-process-promise";
 // bump the package version (assumed to be [currentval] + 0.1.0
 export function bumpVersion(path, releaseType, callback) {
   console.log("\nGot to the bump call:", releaseType);
-  try {
-	  let ret = bumpPackageVersion(path, releaseType);
-	  callback(ret.error ? null : ret.error, ret.data);
-  } catch (e) {
-	  callback(e);
-  }
+
+	let ret = bumpPackageVersion(path, releaseType);
+
+  ret.then( (result) => {
+    callback(null, result);
+  }, (error) => {
+    // Error propogates back up.
+    callback(error);
+  }).catch( (exception) => {
+    // Exception block
+    console.error(exception);
+    callback(exception);
+  });
+  
 };
 
 // commit the project
 export function commit(message, callback) {
 
   // Using Promises
-  commitChangesLocally(message ? message : "Automated commit from bump-tool")
+  commitChangesLocally(message)
     .then( (result) => { // success
       // The result is a childProcess object
-      console.log(`git commited successfully with status code:
-                  ${result["childProcess"]["exitCode"]}`);
+      console.log(`
+      git commited successfully with status code: ${result["childProcess"]["exitCode"]}`);
       // Is this desired functionality?
       return callback(null, result);
     }, (err) => { // failure
@@ -37,14 +45,37 @@ export function commit(message, callback) {
     });
 };
 
-export function tag(callback) {
+export function tag(versionString, tagPushMessage, callback) {
   // TODO: add tags through git command
-  // TODO: potentially, derive tag version with package.json version
+  versionTag(versionString, tagPushMessage)
+    .then( (result) => {
+      // pretty write something to inform the user they did goo
+      callback (null,result);
+    }, (err) => {
+      // Console.Error notifiction
+      callback(err);
+    })
+    .catch( (exc) => {
+      // log the exception
+      // callback with the exception
+    });
 };
 
 export function pushTags(callback) {
   let res = tagPush();
-  callback(res.error);
+
+  res.then(
+    (result) => {
+      console.log("Tag push was successful");
+      callback(null, result);
+    },
+    (error) => {
+      console.error(`ERROR: ${error}`);
+      callback(error);
+    })
+    .catch((ex) => {
+      console.error(`Exception: ${ex}`);
+    });
 };
 
 /********************* Actual working functions *********************/
@@ -99,7 +130,7 @@ function bumpPackageVersion(pathToPackageJSON, bumpType) {
 			              else {
 			                console.log("Write was successful");
                       // resolve data
-                      resolve(data);
+                      resolve(newVer);
 			              }
 		              });
 	    }	
@@ -135,16 +166,38 @@ function commitChangesLocally(commitMessage) {
   });
 };
 
+function versionTag(version, tagMessage) {
+
+  new Promise (function (resolve, reject) {
+    // probe the file for the version
+    // tag with said version: "git tag -a <version> -m <>"
+    exec(`git tag -a v${version} -m "${tagMessage}"`)
+      .then((succRes) => {
+        console.log(`Version tagging to v${version} was succesful`);
+        resolve(succRes);
+      }, (failRes) => {
+        console.error(`ERROR: Tagging to v${version} FAILED`);
+        reject(new Error(failRes));
+      })
+      .catch( (exception) => {
+        console.error(`ERROR: ${exception}`);
+        reject(new Error(exception));
+      });
+  });
+}
+
 //push git tags to remote repository
 function tagPush() {
 
   new Promise (function (resolve, reject) {
     exec("git push --tags")
       .then(function (result) {
-        // 
+        //
+        resolve(result);
       })
       .fail(function (error) {
-        // 
+        //
+        reject(error);
       });    
   });
-}
+};
