92146ca913c2694e44470f53074226c756c89339
Stephen Fox <stevemasta34@gmail.com>
2015-07-17 14:47:26 -0600

    Mocha test commit: commit from the testing frame work

--- test/io-tests.js
+++ test/io-tests.js
@@ -2,32 +2,43 @@
 var chai = require("chai");
 var expect = chai.expect;
 var myIo = require("../lib/io.js");
-
+var exec = require("child-process-promise").exec;
 
 describe("IO", function() {
   describe("#commit()", function() {
     it("should perfom a git commit, with message", function () {
       console.log("Initializing test");
-      var commitMessage = "Mocha test commit: ";
+      var commitMessage = "Mocha test commit: commit from the testing frame work";
       var self = this;
       var _this = this;
-      var doAssertion = function () {
-        expect(self).to.have.a.property("result", _this.res);
-      };
-      
-      var testCallback = function (err, res) {
-        _this.res = res;
-        if (err) expect(0).to.be.same(1); // force fail
 
-        self.result = res;
-        expect(self).to.have.a.property("result", res);
-
-        console.log(self.result);
-        doAssertion();
-      };
-
-      myIo.commit(commitMessage, testCallback);
-
+      myIo.commit(commitMessage)
+        .then(function () {
+          self.res = 0;
+        })
+        .then(function () {
+          exec("git reset HEAD^")
+            .then(function (result) {
+              console.log("Undid the commit");
+              
+              // Let's look at the output streams.
+              var stdout = result.stdout;
+              var stderr = result.stderr;
+              console.log('stdout: ', stdout);
+              console.log('stderr: ', stderr);
+            })
+            .fail(function (err) {
+              console.error("ERROR: ",err);
+            })
+            .progress(function (childProcess) {
+              console.log("childProcess.pid = ",childProcess.pid);
+            });
+        })
+        .catch(function (err) {
+          console.log("Error block, pseudo-failure");
+          // break the test
+          expect(self).to.have.a.property("res", 100);
+        });
     });
   });
 });
